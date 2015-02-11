eda4a160f1160ff44676a5c2a8bf10bc83077c54
uday <uday@intelligrape.com>
2015-02-11 15:46:25 +0530

    Remvoed unused files from git

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,19 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+    username = "root"
     password = ""
-    loggingSql = true
+    properties {
+        maxActive = 40
+        initialSize = 20
+        minIdle = 5
+        numTestsPerEvictionRun = 3
+        testWhileIdle = true
+        validationQuery = "SELECT 1"
+        minEvictableIdleTimeMillis = (1000 * 60 * 5)
+        timeBetweenEvictionRunsMillis = (1000 * 60 * 5)
+    }
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -16,8 +26,11 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            username = "root"
+            password = "igdefault"
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/gorm1?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
+            logSql = true
         }
     }
     test {
@@ -31,15 +44,15 @@ environments {
             dbCreate = "update"
             url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=false
-               validationQuery="SELECT 1"
-               jdbcInterceptors="ConnectionState"
+                maxActive = -1
+                minEvictableIdleTimeMillis = 1800000
+                timeBetweenEvictionRunsMillis = 1800000
+                numTestsPerEvictionRun = 3
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                validationQuery = "SELECT 1"
+                jdbcInterceptors = "ConnectionState"
             }
         }
     }
